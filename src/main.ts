5fd96b31532c1e1c68ca17c0d1aa474549927e7d
Aiman Hakim <aiman.azahari@tmrnd.com.my>
2026-03-02 15:12:44 +0800

    fix(main.ts): enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:4200',
+      'http://localhost:3000',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With'],
+    exposedHeaders: ['Content-Range', 'X-Content-Range'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
